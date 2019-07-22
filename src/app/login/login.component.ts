5d0ff9c4a7d1284fcdad68078a556b3705512833
ivan1373 <34128933+ivan1373@users.noreply.github.com>
2019-07-22 16:40:32 +0200

    guard, interceptors, some modifications

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  returnUrl: any;
+
+  constructor(private router: Router,
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    /*private as: AuthService*/) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [
+        Validators.required,
+      ]],
+      password: ['', [
+        Validators.required
+      ]]
+    })
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  toRegister() {
+    this.router.navigate(['/register']);
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  loginUser()
+  {
+        console.log(this.f.email.value, this.f.password.value);
+        // stop here if form is invalid
+       /* if (this.loginForm.invalid) {
+            return;
+        }
+
+  
+        this.as.login(this.f.email.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    console.log(error);
+                });*/
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router, /*private as: AuthService*/) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      password_confirmation: ['', [Validators.required, Validators.minLength(6)]]
+  });
+  }
+
+  toLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  registerUser() {
+    console.log(this.registerForm.value);
+    // stop here if form is invalid
+    /*if (this.registerForm.invalid) {
+      return;
+    	}
+
+    this.as.register(this.registerForm.value)
+      .subscribe(
+          data => {
+              //this.alertService.success('Registration successful', true);
+              console.log('success!')
+              this.router.navigate(['/login']);
+          },
+          error => {
+              //this.alertService.error(error);
+              console.log('success! ...not!')
+          });*/
   }
 
 }
