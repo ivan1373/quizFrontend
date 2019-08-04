3f04f48c98d65a2050c4df7b6ee6f0e4ec75c63c
Ivan Miloš <34128933+ivan1373@users.noreply.github.com>
2019-08-05 00:05:16 +0200

    question and options multi row store

--- src/app/add-question/add-question.component.ts
+++ src/app/add-question/add-question.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup, Validators } from  '@angular/forms';  
+import { MatRadioChange, MatRadioButton } from '@angular/material/radio';
 
 @Component({
   selector: 'app-add-question',
@@ -9,10 +11,51 @@ export class AddQuestionComponent implements OnInit {
 
   selectedNumOfOptions: number = 0;
   options: string[] = ['2','3','4'];
+  
 
-  constructor() { }
+
+  questionForm : FormGroup;
+
+  constructor(private fb :FormBuilder) { }
 
   ngOnInit() {
+    this.createform();    
   }
 
+  handleChange(mrChange: MatRadioChange) {
+    this.createform();
+ } 
+
+  createform()    
+  {    
+  let arr=[];    
+  for(let i=0;i< this.selectedNumOfOptions;i++)    
+  {       
+    arr.push(this.BuildFormDynamic())
+       
+  }    
+
+    this.questionForm =  this.fb.group({    
+      description : ['', [
+        Validators.required,
+      ]],    
+      optionDetails:this.fb.array(arr)    
+    })    
+  }  
+
+  BuildFormDynamic():FormGroup{    
+    return this.fb.group({       
+          option: ['', [
+            Validators.required,
+          ]],      
+     })    
+   }    
+
+   SaveData()    
+  {    
+    console.log(this.questionForm.value);    
+    //pass this data to service and api node/webapi  
+  
+  }   
+    
 }
